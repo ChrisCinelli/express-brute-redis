6d3ce423f355c2708e2c545eb868212b41c200db
Chris Cinelli <ccinelli@ebay.com>
2017-08-01 14:20:27 -0700

    Updates to support whitelisting and blacklisting

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var AbstractClientStore = require('express-brute/lib/AbstractClientStore'),
     Redis = require('redis'),
-    _ = require('underscore');
+    _ = require('underscore'),
+    redisScan = require('redisscan');
+
 
 var RedisStore = module.exports = function (options) {
 	AbstractClientStore.apply(this, arguments);
@@ -35,6 +37,54 @@ RedisStore.prototype.set = function (key, value, lifetime, callback) {
 		typeof callback == 'function' && callback.call(this, null);
 	});
 };
+
+RedisStore.prototype.getKeys = function (opts, callback) {
+  var ret = {};
+  redisScan({
+      redis: this.client,
+      pattern: opts.pattern || '',
+      keys_only: opts.keys_only,
+      each_callback: function (type, key, subkey, length, value, cb) {
+          if (!opts.fnShouldReturn) {
+            ret[key] = value;
+          } else {
+            var v = opts.fnShouldReturn(key, value);
+            if (v !== false) ret[key] = v;
+          }
+          cb();
+      },
+      done_callback: function (err) {
+          if (err) return callback(err);
+          callback(null, ret);
+      }
+  });
+};
+
+RedisStore.prototype.getBlAndWlKeys = function (callback) {
+  var wl = [], bl = [];
+  redisScan({
+      redis: this.client,
+      pattern: (this.options.prefix || '') + '*',
+      keys_only: false,
+      each_callback: function (type, key, subkey, length, value, cb) {
+          var v = {}
+          try {
+            v = JSON.parse(value);
+          } catch (e) {
+            // No problem
+          }
+          if(v.bl && v.ip) bl.push(v.ip);
+          if(v.wl && v.ip) wl.push(v.ip);
+          
+          cb();
+      },
+      done_callback: function (err) {
+          if (err) return callback(err);
+          callback(null, {wl: wl, bl: bl});
+      }
+  });
+};
+
 RedisStore.prototype.get = function (key, callback) {
 	this.client.get(this.options.prefix+key, function (err, data) {
 		if (err) {
